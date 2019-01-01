3ddf61b7aef9e395a319297d43c515ae77585ebd
IsaiasMorochi <isaiasmorochi95@gmail.com>
2019-01-01 19:40:16 -0400

    .

--- src/main/java/com/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/springboot/app/controllers/ClienteController.java
@@ -17,61 +17,73 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @SessionAttributes("cliente")
 public class ClienteController {
 
-    @Autowired
-    private IClienteService clienteService;
+	@Autowired
+	private IClienteService clienteService;
 
-    @RequestMapping(value = "/listar", method = RequestMethod.GET)
-    public String listar(Model model){
-        model.addAttribute("titulo", "Listado de Clientes");
-        model.addAttribute("clientes", clienteService.findAll());
-        return "listar";  // nombre de la vista
-    }
-    
-    @RequestMapping(value = "/form")
-    public String crear(Map<String, Object> model) {
-    	Cliente cliente = new Cliente();
-    	model.put("cliente", cliente);
-    	model.put("titulo", "Formulario de Cliente");
+	@RequestMapping(value = "/listar", method = RequestMethod.GET)
+	public String listar(Model model) {
+		model.addAttribute("titulo", "Listado de clientes");
+		model.addAttribute("clientes", clienteService.findAll());
+		return "listar";
+	}
+
+	@RequestMapping(value = "/form")
+	public String crear(Map<String, Object> model) {
+
+		Cliente cliente = new Cliente();
+		model.put("cliente", cliente);
+		model.put("titulo", "Crear Cliente");
 		return "form";
-    }
-    
-    @RequestMapping(value = "/form", method=RequestMethod.POST)
-    public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
-    	
-    	if(result.hasErrors()) {
-    		model.addAttribute("titulo", "Formulario de Cliente"); 
-    		return "form";
-    	}
-    	
-    	clienteService.save(cliente);
-    	status.setComplete();
-    	return "redirect:listar";
-    }
-    
-    @RequestMapping(value="/form/{id}")
-	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+	}
+	
+	@RequestMapping(value="/form/{id}")
+	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 		
 		Optional<Cliente> cliente = null;
 		
 		if(id > 0) {
-			cliente =  clienteService.findOne(id);
+			cliente = clienteService.findOne(id);
+			if(cliente == null) {
+				flash.addFlashAttribute("error", "El ID del cliente no existe en la BBDD!");
+				return "redirect:/listar";
+			}
 		} else {
+			flash.addFlashAttribute("error", "El ID del cliente no puede ser cero!");
 			return "redirect:/listar";
 		}
 		model.put("cliente", cliente);
 		model.put("titulo", "Editar Cliente");
 		return "form";
 	}
-    
-	@RequestMapping(value="/eliminar/{id}")
-	public String eliminar(@PathVariable(value="id") Long id) {
-		if(id > 0) clienteService.delete(id);
-		return "redirect:/listar";
+
+	@RequestMapping(value = "/form", method = RequestMethod.POST)
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
+
+		if (result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario de Cliente");
+			return "form";
+		}
+		String mensajeFlash = (cliente.getId() != null)? "Cliente editado con éxito!" : "Cliente creado con éxito!";
+
+		clienteService.save(cliente);
+		status.setComplete();
+		flash.addFlashAttribute("success", mensajeFlash);
+		return "redirect:listar";
 	}
-    
+	
+	@RequestMapping(value="/eliminar/{id}")
+	public String eliminar(@PathVariable(value="id") Long id, RedirectAttributes flash) {
+		
+		if(id > 0) {
+			clienteService.delete(id);
+			flash.addFlashAttribute("success", "Cliente eliminado con éxito!");
+		}
+		return "redirect:/listar";
+	}  
 }
--- src/main/java/com/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/springboot/app/models/dao/IClienteDao.java
@@ -1,8 +1,9 @@
 package com.springboot.app.models.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 import com.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long> {
+public interface IClienteDao extends PagingAndSortingRepository<Cliente, Long> {
 
 }
