f144e797fb2b4477d8621c451d57004db5117a45
IsaiasMorochi <isaiasmorochi95@gmail.com>
2019-09-03 07:58:43 -0400

    manejo de errores AccesDeniedPage

--- src/main/java/com/config/MvcConfig.java
+++ src/main/java/com/config/MvcConfig.java
@@ -3,6 +3,7 @@ package com.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.Paths;
@@ -31,4 +32,9 @@ public class MvcConfig implements WebMvcConfigurer{
 		registry.addResourceHandler("/uploads/**")
 		.addResourceLocations(resourcePath);
 	}*/
+
+	public void addViewControllers(ViewControllerRegistry registry){
+		registry.addViewController("/error_403").setViewName("error_403");
+	}
+
 }
--- src/main/java/com/config/SpringSecurityConfig.java
+++ src/main/java/com/config/SpringSecurityConfig.java
@@ -49,7 +49,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin()
             .loginPage("/login").permitAll()
             .and()
-            .logout().permitAll();
+            .logout().permitAll()
+            .and()
+            .exceptionHandling().accessDeniedPage("/error_403");
     }
 
 
