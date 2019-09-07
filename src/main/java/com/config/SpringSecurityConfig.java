4e4bc52320acfde7dd0b130588a9027694323d82
IsaiasMorochi <isaiasmorochi95@gmail.com>
2019-09-06 20:24:02 -0400

    Implementando la clase SuccesHandler paa personalizar el mensaje de exito

--- src/main/java/com/config/SpringSecurityConfig.java
+++ src/main/java/com/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.config;
 
+import com.auth.handler.LoginSuccesHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private LoginSuccesHandler succesHandler;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
@@ -46,8 +50,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/factura/**").hasAnyRole("ADMIN")
             .anyRequest().authenticated()
             .and()
-            .formLogin()
-            .loginPage("/login").permitAll()
+                .formLogin()
+                .successHandler(succesHandler)
+                .loginPage("/login")
+                .permitAll()
             .and()
             .logout().permitAll()
             .and()
