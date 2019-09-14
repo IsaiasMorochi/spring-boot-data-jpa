f32c95f859e74dbe2408a7d0da74df581be038c8
IsaiasMorochi <isaiasmorochi95@gmail.com>
2019-09-14 11:40:40 -0400

    Implementando la clase de vista FacturaXlsView para la factura

--- src/main/java/com/view/xls/FacturaXlsxView.java
+++ src/main/java/com/view/xls/FacturaXlsxView.java
@@ -1,6 +1,10 @@
 package com.view.xls;
 
 
+import com.models.entity.Factura;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
@@ -13,7 +17,31 @@ import java.util.Map;
 public class FacturaXlsxView extends AbstractXlsxView {
 
     @Override
-    protected void buildExcelDocument(Map<String, Object> map, Workbook workbook, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        Factura factura = (Factura) model.get("factura");
+
+        Sheet sheet = workbook.createSheet("Factura Spring"); //planilla
+
+        Row row = sheet.createRow(0); //fila
+
+        Cell cell = row.createCell(0); //celda
+
+        cell.setCellValue("Datos del Cliente");
+
+        row = sheet.createRow(1);
+        cell = row.createCell(0);
+        cell.setCellValue(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
+
+        row = sheet.createRow(2);
+        cell = row.createCell(0);
+        cell.setCellValue(factura.getCliente().getEmail());
+
+        sheet.createRow(4).createCell(0).setCellValue("Datos de la Factura");
+        sheet.createRow(5).createCell(0).setCellValue("Folio: " + factura.getId());
+        sheet.createRow(6).createCell(0).setCellValue("Descripcion: " + factura.getDescripcion());
+        sheet.createRow(7).createCell(0).setCellValue("Fecha: " + factura.getCreateAt());
+
 
     }
 }
