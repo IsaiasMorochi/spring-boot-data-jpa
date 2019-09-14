88392db562a73c85ab83b15e67c5054eb246688a
IsaiasMorochi <isaiasmorochi95@gmail.com>
2019-09-13 22:09:28 -0400

    Traduccion completa de nuestro aplicacion y su descarga

--- src/main/java/com/auth/handler/LoginSuccesHandler.java
+++ src/main/java/com/auth/handler/LoginSuccesHandler.java
@@ -1,32 +1,42 @@
 package com.auth.handler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.support.SessionFlashMapManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Locale;
 
 @Component
 public class LoginSuccesHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
+    private LocaleResolver localeResolver;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
         SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
         FlashMap flashMap = new FlashMap();
+        Locale locale = localeResolver.resolveLocale(request);
+        String mensaje = String.format(messageSource.getMessage("text.login.success", null, locale), authentication.getName());
 
-        flashMap.put("success", "Hola, " + authentication.getName() + " haz iniciado sesion con exito");
-        flashMapManager.saveOutputFlashMap(flashMap,request, response);
-
-        if (authentication != null){
-            logger.info("El usuario, '"+ authentication.getName() +"' ha iniciado sesion con exito");
+        flashMap.put("success", mensaje);
+        flashMapManager.saveOutputFlashMap(flashMap, request, response);
+        if(authentication != null) {
+            logger.info(mensaje);
         }
-
         super.onAuthenticationSuccess(request, response, authentication);
     }
 }
--- src/main/java/com/controllers/FacturaController.java
+++ src/main/java/com/controllers/FacturaController.java
@@ -8,6 +8,7 @@ import com.models.service.IClienteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +19,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 @Secured("ROLE_ADMIN")
@@ -29,42 +31,37 @@ public class FacturaController {
     @Autowired
     private IClienteService clienteService;
 
+    @Autowired private MessageSource messageSource;
+
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @GetMapping("/ver/{id}")
     public String ver(@PathVariable(value="id") Long id,
                       Model model,
-                      RedirectAttributes flash) {
+                      RedirectAttributes flash, Locale locale) {
         Factura factura = clienteService.fetchByIdWithClienteWithItemFacturaWithProducto(id); //clienteService.findFacturaById(id);
-
         if(factura == null) {
-            flash.addFlashAttribute("error", "La factura no existe en la base de datos!");
+            flash.addFlashAttribute("error", messageSource.getMessage("text.factura.flash.db.error", null, locale));
             return "redirect:/listar";
         }
-
         model.addAttribute("factura", factura);
-        model.addAttribute("titulo", "Factura: ".concat(factura.getDescripcion()));
-
+        model.addAttribute("titulo", String.format(messageSource.getMessage("text.factura.ver.titulo", null, locale), factura.getDescripcion()));
         return "factura/ver";
     }
 
     @GetMapping("/form/{clienteId}")
     public String crear(@PathVariable(value = "clienteId") Long clienteId, Map<String, Object> model,
-                        RedirectAttributes flash) {
+                        RedirectAttributes flash, Locale locale) {
 
         Cliente cliente = clienteService.findById(clienteId);
-
         if (cliente == null) {
-            flash.addFlashAttribute("error", "El cliente no existe en la base de datos");
+            flash.addFlashAttribute("error", messageSource.getMessage("text.cliente.flash.db.error", null, locale));
             return "redirect:/listar";
         }
-
         Factura factura = new Factura();
         factura.setCliente(cliente);
-
         model.put("factura", factura);
-        model.put("titulo", "Crear Factura");
-
+        model.put("titulo", messageSource.getMessage("text.factura.form.titulo", null, locale));
         return "factura/form";
     }
 
@@ -80,48 +77,47 @@ public class FacturaController {
                           @RequestParam(name = "item_id[]", required = false) Long[] itemId,
                           @RequestParam(name = "cantidad[]", required = false) Integer[] cantidad,
                           RedirectAttributes flash,
-                          SessionStatus status) {
+                          SessionStatus status,
+                          Locale locale) {
 
         if (result.hasErrors()) {
-            model.addAttribute("titulo", "Crear Factura");
+            model.addAttribute("titulo", messageSource.getMessage("text.factura.form.titulo", null, locale));
             return "factura/form";
         }
 
         if (itemId == null || itemId.length == 0) {
-            model.addAttribute("titulo", "Crear Factura");
-            model.addAttribute("error", "Error: La factura NO puede no tener líneas!");
+            model.addAttribute("titulo", messageSource.getMessage("text.factura.form.titulo", null, locale));
+            model.addAttribute("error", messageSource.getMessage("text.factura.flash.lineas.error", null, locale));
             return "factura/form";
         }
 
         for (int i = 0; i < itemId.length; i++) {
             Producto producto = clienteService.findProductoById(itemId[i]);
-
             ItemFactura linea = new ItemFactura();
             linea.setCantidad(cantidad[i]);
             linea.setProducto(producto);
             factura.addItemFactura(linea);
-
             log.info("ID: " + itemId[i].toString() + ", cantidad: " + cantidad[i].toString());
         }
 
         clienteService.saveFactura(factura);
         status.setComplete();
 
-        flash.addFlashAttribute("success", "Factura creada con éxito!");
+        flash.addFlashAttribute("success", messageSource.getMessage("text.factura.flash.crear.success", null, locale));
 
         return "redirect:/ver/" + factura.getCliente().getId();
     }
 
     @GetMapping("/eliminar/{id}")
-    public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash){
-
+    public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash, Locale locale){
         Factura factura = clienteService.findFacturaById(id);
-        if (factura != null){
+        if (factura != null) {
             clienteService.deleteFactura(id);
-            flash.addFlashAttribute("success", "Factura eliminada con éxito!");
+            flash.addFlashAttribute("success", messageSource.getMessage("text.factura.flash.eliminar.success", null, locale));
             return "redirect:/ver/" + factura.getCliente().getId();
         }
-        flash.addFlashAttribute("error", "No existe la factura en la base de datos, no se puede eliminar!");
+        flash.addFlashAttribute("error", messageSource.getMessage("text.factura.flash.db.error", null, locale));
+
         return "redirect:/listar";
     }
 
