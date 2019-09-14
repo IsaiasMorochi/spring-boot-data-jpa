1d786f9f94fbfe3e5392f30f5d9699b54b62cf04
IsaiasMorochi <isaiasmorochi95@gmail.com>
2019-09-13 22:55:58 -0400

    Escribiendo el codigo de las lineas de la FACTURA en la vista PDF

--- src/main/java/com/view/pdf/FacturaPdfView.java
+++ src/main/java/com/view/pdf/FacturaPdfView.java
@@ -2,10 +2,13 @@ package com.view.pdf;
 
 
 import com.lowagie.text.Document;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfTable;
 import com.lowagie.text.pdf.PdfWriter;
 import com.models.entity.Factura;
+import com.models.entity.ItemFactura;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
@@ -23,11 +26,11 @@ public class FacturaPdfView extends AbstractPdfView {
 
         Factura factura = (Factura) map.get("factura");
 
-        PdfPTable tabla = new PdfPTable(1);
-        tabla.setSpacingAfter(20);
-        tabla.addCell("Datos del cliente");
-        tabla.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
-        tabla.addCell(factura.getCliente().getEmail());
+        PdfPTable tabla1 = new PdfPTable(1);
+        tabla1.setSpacingAfter(20);
+        tabla1.addCell("Datos del cliente");
+        tabla1.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
+        tabla1.addCell(factura.getCliente().getEmail());
 
         PdfPTable tabla2 = new PdfPTable(1);
         tabla2.setSpacingAfter(20);
@@ -36,7 +39,28 @@ public class FacturaPdfView extends AbstractPdfView {
         tabla2.addCell("Descripcion: " + factura.getDescripcion());
         tabla2.addCell("Fecha: " + factura.getCreateAt());
 
-        document.add(tabla);
+        document.add(tabla1);
         document.add(tabla2);
+
+        PdfPTable tabla3 = new PdfPTable(4);
+        tabla3.addCell("Producto");
+        tabla3.addCell("Precio");
+        tabla3.addCell("Cantidad");
+        tabla3.addCell("Total");
+
+        for (ItemFactura item: factura.getItems()){
+            tabla3.addCell(item.getProducto().getNombre());
+            tabla3.addCell(item.getProducto().getPrecio().toString());
+            tabla3.addCell(item.getCantidad().toString());
+            tabla3.addCell(item.calcularImporte().toString());
+        }
+
+        PdfPCell cell = new PdfPCell(new Phrase("Total: "));
+        cell.setColspan(3); //ocupe 3 columnas
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+        tabla3.addCell(cell);
+        tabla3.addCell(factura.getTotal().toString());
+
+        document.add(tabla3);
     }
 }
